a9b017574ca3195fe493ed88cc3522b964183f16
Adrian Berisha <adrianberisha0@gmail.com>
2025-01-28 00:47:56 +0100

    staticfiles

--- portfolio_py/urls.py
+++ portfolio_py/urls.py
@@ -7,3 +7,9 @@ urlpatterns = [
     path('admin', admin.site.urls),
     path("", include("portfolio_app.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
