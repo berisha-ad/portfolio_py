a4781e5049e998c63444df6dde3dc5f5de8d92c5
Adrian Berisha <adrianberisha0@gmail.com>
2025-01-30 06:19:22 +0100

    bugfix

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -20,7 +20,7 @@ def projects(request):
 
     for sinproject in projects:
         sinproject.skills_list = [
-            skill.strip() for skill in project.skills.split(",") if skill.strip()]
+            skill.strip() for skill in sinproject.skills.split(",") if skill.strip()]
 
     return render(request, "portfolio/projects.html", {
         "projects": projects
